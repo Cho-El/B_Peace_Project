3434b72d455b1a0ba49e44dfc008e220bb975ffd
Cho-El <syzzang21c@gmail.com>
2022-07-01 14:20:51 +0900

    2022-07-01
    장고 리액트 환경설정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,4 @@
-"""B_Peace_Project URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
